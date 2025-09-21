7d1b043c9e55f2b7b9b6ab961825d66937ff9642
Kur03103 <aayushaj3103@gmail.com>
2025-09-21 16:03:19 +0545

    Add more git commands tutorials

--- scrape.py
+++ scrape.py
@@ -11,6 +11,13 @@
 #git add .
 #git commit -m "Finish Project"
 # copy paste git code from github
+
+###############
+# 1. change the code
+# 2. git add
+# 3. git commit -m "Your message"
+# 4. git push
+
 import json
 import requests 
 from bs4 import BeautifulSoup
